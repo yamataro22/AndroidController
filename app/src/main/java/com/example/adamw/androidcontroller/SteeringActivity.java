b6d47c1050772dd730b3d53fc9002ec6a6211393
siny955 <siny955@wp.pl>
2018-12-14 23:48:18 +0100

    Added enable_switch for steering and green/red connection indicator with temporary "check switch"

--- app/src/main/java/com/example/adamw/androidcontroller/SteeringActivity.java
+++ app/src/main/java/com/example/adamw/androidcontroller/SteeringActivity.java
@@ -6,8 +6,15 @@ import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.SurfaceView;
+import android.view.View;
+import android.widget.Switch;
+import android.widget.TextView;
 
 public class SteeringActivity extends Activity implements JoystickView.JoystickListener{
+
+    private TextView mDioda;
+    private JoystickView mFirstJoystick;
+    private JoystickView mSecondJoystick;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -20,4 +27,43 @@ public class SteeringActivity extends Activity implements JoystickView.JoystickL
     public void onJoystickMoved(float xPercent, float yPercent, int source) {
         Log.i("dimensions:" , xPercent+" "+yPercent+ " ");
     }
+
+    //probny switch czy dziala zmienianie koloru
+    public void onSwitchClicked(View view) {
+
+        boolean on = ((Switch) view).isChecked();
+
+        //w naszym wypadku bedzie to: "jesli polaczono"
+        if (on){
+            mDioda.setBackgroundResource(R.drawable.circle);
+        }
+
+        //w naszym wypadku bedzie to: "jesli nie polaczono"
+        else
+        {
+            mDioda.setBackgroundResource(R.drawable.circle2);
+        }
+    }
+
+    public void onEnableSwitchClicked(View view) {
+
+        boolean on = ((Switch) view).isChecked();
+
+        //w naszym wypadku bedzie to: "odblokuj joysticki"
+        if (on){
+            enableJoysticks(true);
+        }
+
+        //w naszym wypadku bedzie to: "zablokuj joysticki"
+        else
+        {
+            enableJoysticks(false);
+        }
+    }
+
+    private void enableJoysticks(boolean enable_value) {
+        mFirstJoystick.setEnabled(enable_value);
+        mSecondJoystick.setEnabled(enable_value);
+    }
+
 }
